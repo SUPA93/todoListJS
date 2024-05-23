2d372f7da99f4352d904425be0d9b1af9b387008
SUPA93 <supaflavaz93@hotmail.com>
2024-05-23 18:36:08 +0200

    added time stamp and some animation

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+// Add class for H1 animation on load
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('h1');
+    header.classList.add('animate-text');
+});
+
 const form = document.querySelector('form');
+const item = document.getElementById('item');
 
 //storage part
 function storeList(){
     window.localStorage.todoList = list.innerHTML;
-
 }
 function getTodos(){
     if (window.localStorage.todoList){
@@ -13,17 +19,32 @@ function getTodos(){
         list.innerHTML = `<li>Cliquez sur un Todo pour le supprimer</li>`
     }
 }
+// Change placeholder on mouseenter
+item.addEventListener('mouseenter', () => {
+    item.placeholder = '...';
+});
+// Restore placeholder 
+item.addEventListener('mouseleave', () => {
+    item.placeholder = 'À faire';
+});
+// Add date for each TODOS
+function addDate(){
+    let now = new Date();
+    return now.toLocaleString();
+}
 //Two ways to call function getTodos()
 window.addEventListener('load', getTodos);
 //getTodos();
 
 // add element
-form.addEventListener("submit", (e)=>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    list.innerHTML += `<li>${item.value}</li>`
+    let formattedDate = addDate();
+    list.innerHTML += `<li>${item.value} - le ${formattedDate}</li>`;
     item.value = "";
     storeList();
 });
+
 
 list.addEventListener("click", (e) => {
     if(e.target.classList.contains("checked")){
